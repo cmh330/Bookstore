2c9d12d4fb03a2b01731d80176d982baa5627bb2
cmh <chenmh25@sjtu.edu.cn>
2025-12-16 21:53:30 +0800

    Add：Account system

---
+++ include/AccountSystem.h
@@ -0,0 +1,74 @@
+//
+// Created by Administrator on 2025/12/8.
+//
+
+#ifndef BOOKSTORE_ACCOUNTSYSTEM_H
+#define BOOKSTORE_ACCOUNTSYSTEM_H
+#include "storage.h"
+#include <vector>
+#include <map>
+
+struct Account {
+    char userID[31];
+    char password[31];
+    char username[31];
+    int privilege;
+};
+
+class AccountSystem {
+private:
+    BlockLinkedList<30, Account>* accountStorage; // userID -> 账户信息
+    std::vector<Account> loginStack; // 记录都有哪些用户登录
+    std::map<string, string> selectedBooks; // userID -> ISBN
+
+    bool isValidPasswordOrID(const string& s);
+    bool isValidUsername(const string& s);
+    bool isValidPrivilege(int privilege);
+
+public:
+    AccountSystem(); // 申请空间
+    ~AccountSystem(); // 释放空间
+
+    // 登录：若成功则修改登录栈
+    // {0}
+    void su(const string& userID, const string& password = "");
+
+    // 登出登录栈中最后一个账户
+    // 若无已登录帐户则操作失败
+    // {1}
+    void logout();
+
+    // 注册权限等级为 {1} 的新账户
+    // 若userID与已注册用户重复则失败
+    // {0}
+    void registerAccount(const string& userID, const string& password, const string& username);
+
+    // 修改密码，权限为{7}账户可以不输入旧密码
+    // 若userID不存在则失败，若旧密码不正确则不存在
+    // {1}
+    void passwd(const string& userID, const string& newPassword, const string& currentPassword = "");
+
+    // 创建账户
+    // 若userID与已注册账户重复则失败，若待创建帐户的权限等级 >= 当前帐户权限等级则失败
+    // {3}
+    void useradd(const string& userID, const string& password, int privilege, const string& username);
+
+    // 删除账户
+    // 若该账户不存在或已登录则失败
+    // {7}
+    void deleteAccount(const string& userID);
+
+    // 设置选中图书
+    void setSelectedBook(const string& ISBN);
+    // 获取选中图书
+    string getSelectedBook() const;
+
+    int getCurrentPrivilege() const;
+    bool hasPrivilege(int required) const;
+    string getCurrentUserID() const;
+
+    bool isUserLoggedIn(const string& userID) const;
+    void initializeRoot();
+};
+
+#endif //BOOKSTORE_ACCOUNTSYSTEM_H
---
+++ src/AccountSystem.cpp
@@ -0,0 +1,325 @@
+//
+// Created by Administrator on 2025/12/8.
+//
+#include "AccountSystem.h"
+#include <iostream>
+#include <cstring>
+
+void AccountSystem::initializeRoot() {
+    vector<Account> nowExisting = accountStorage->Find("root");
+    if (!nowExisting.empty()) {
+        return;
+    }
+    Account root;
+    strcpy(root.userID, "root");
+    strcpy(root.password, "sjtu");
+    strcpy(root.username, "root");
+    root.privilege = 7;
+    accountStorage->Insert("root", root);
+}
+
+
+
+// ========== 构造 & 析构 ==========
+AccountSystem::AccountSystem() {
+    accountStorage = new BlockLinkedList<30, Account>("account");
+    loginStack.clear();
+    selectedBooks.clear();
+
+    initializeRoot();
+}
+AccountSystem::~AccountSystem() {
+    delete accountStorage;
+}
+
+
+
+// =========== 辅助 ============
+bool AccountSystem::isValidPasswordOrID(const string& s) {
+    if (s.empty() || s.length() > 30) {
+        return false;
+    }
+    for (char c : s) {
+        if (!((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_'))) {
+            return false;
+        }
+    }
+    return true;
+}
+bool AccountSystem::isValidUsername(const string& s) {
+    if (s.empty() || s.length() > 30) {
+        return false;
+    }
+    for (char c : s) {
+        if (!(c >= 32 && c <= 126)) {
+            return false;
+        }
+    }
+    return true;
+}
+bool AccountSystem::isValidPrivilege(int privilege) {
+    if (!(privilege == 7 || privilege == 5 || privilege == 3 || privilege == 0)) {
+        return false;
+    }
+    return true;
+}
+int AccountSystem::getCurrentPrivilege() const {
+    if (loginStack.empty()) {
+        return 0;
+    }
+    return loginStack.back().privilege;
+}
+bool AccountSystem::hasPrivilege(int required) const {
+    if (loginStack.empty()) {
+        return 0;
+    }
+    return (loginStack.back().privilege >= required);
+}
+string AccountSystem::getCurrentUserID() const {
+    if (loginStack.empty()) {
+        return "";
+    }
+    return loginStack.back().userID;
+}
+
+void AccountSystem::setSelectedBook(const string& ISBN) {
+    if (loginStack.empty()) {
+        return;
+    }
+    string userID = getCurrentUserID();
+    selectedBooks[userID] = ISBN;
+}
+string AccountSystem::getSelectedBook() const {
+    if (loginStack.empty()) {
+        return "";
+    }
+    string userID = getCurrentUserID();
+    auto it = selectedBooks.find(userID);
+    if (it == selectedBooks.end()) {
+        return "";
+    }
+    return it->second;
+}
+bool AccountSystem::isUserLoggedIn(const string& userID) const {
+    for (int i = 0; i < loginStack.size(); ++i) {
+        if (strcmp(loginStack[i].userID, userID.c_str()) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+// ============ 登录 ============
+void AccountSystem::su(const string& userID, const string& password) {
+    if (!isValidPasswordOrID(userID)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    if (!password.empty() && !isValidPasswordOrID(password)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+
+    vector<Account> nowExisting = accountStorage->Find(userID);
+    if (nowExisting.empty()) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    Account account = nowExisting[0];
+    int currentPrivilege = getCurrentPrivilege();
+    if (currentPrivilege > account.privilege) {
+        loginStack.push_back(account);
+        return;
+    }
+    if (password.empty()) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    if (strcmp(account.password, password.c_str()) == 0) {
+        loginStack.push_back(account);
+        return;
+    } else {
+        std::cout << "Invalid\n";
+        return;
+    }
+}
+
+
+// ============ 登出 ============
+void AccountSystem::logout() {
+    if (loginStack.empty()) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    if (!hasPrivilege(1)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    string userID = loginStack.back().userID;
+    loginStack.pop_back();
+    selectedBooks.erase(userID);
+}
+
+
+// ============ 注册 ============
+void AccountSystem::registerAccount(const string& userID, const string& password, const string& username) {
+    if (!isValidPasswordOrID(userID)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    if (!isValidPasswordOrID(password)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    if (!isValidUsername(username)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+
+    vector<Account> nowExisting = accountStorage->Find(userID);
+    if (!nowExisting.empty()) {
+        std::cout << "Invalid\n";
+        return;
+    }
+
+    Account newAccount;
+    strcmp(newAccount.username, username.c_str());
+    strcmp(newAccount.password, password.c_str());
+    strcpy(newAccount.userID, userID.c_str());
+    newAccount.privilege = 1;
+    accountStorage->Insert(userID, newAccount);
+}
+
+
+// ============ 修改密码 ============
+void AccountSystem::passwd(const string& userID, const string& newPassword, const string& currentPassword) {
+    if (!isValidPasswordOrID(newPassword)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    if (!currentPassword.empty() && !isValidPasswordOrID(currentPassword)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    if (!isValidPasswordOrID(userID)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    if (!hasPrivilege(1)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+
+    vector<Account> nowExisting = accountStorage->Find(userID);
+    if (nowExisting.empty()) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    Account account = nowExisting[0];
+    int currentPrivilege = getCurrentPrivilege();
+    if (currentPrivilege == 7) {
+        Account updatedAccount = account;
+        strcpy(updatedAccount.password, newPassword.c_str());
+        accountStorage->Delete(userID, account);
+        accountStorage->Insert(userID, updatedAccount);
+
+        for (int i = 0; i < loginStack.size(); ++i) {
+            if (strcmp(loginStack[i].userID, userID.c_str()) == 0) {
+                loginStack[i] = updatedAccount;
+            }
+        }
+        return;
+    }
+
+    if (currentPassword.empty()) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    if (strcmp(account.password, currentPassword.c_str()) != 0) {
+        std::cout << "Invalid\n";
+        return;
+    } else {
+        Account updatedAccount = account;
+        strcpy(updatedAccount.password, newPassword.c_str());
+        accountStorage->Delete(userID, account);
+        accountStorage->Insert(userID, updatedAccount);
+
+        for (int i = 0; i < loginStack.size(); ++i) {
+            if (strcmp(loginStack[i].userID, userID.c_str()) == 0) {
+                loginStack[i] = updatedAccount;
+            }
+        }
+        return;
+    }
+}
+
+
+// ============ 创建账户 ============
+void AccountSystem::useradd(const string& userID, const string& password, int privilege, const string& username) {
+    if (!isValidUsername(username)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    if (!isValidPasswordOrID(userID)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    if (!isValidPasswordOrID(password)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    if (!isValidPrivilege(privilege)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    if (!hasPrivilege(3)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+
+    vector<Account> nowExisting = accountStorage->Find(userID);
+    if (!nowExisting.empty()) {
+        std::cout << "Invalid\n";
+        return;
+    }
+
+    int currentPrivilege = getCurrentPrivilege();
+    if (privilege >= currentPrivilege) {
+        std::cout << "Invalid\n";
+        return;
+    }
+
+    Account newAccount;
+    strcpy(newAccount.username, username.c_str());
+    strcpy(newAccount.password, password.c_str());
+    newAccount.privilege = privilege;
+    strcpy(newAccount.userID, userID.c_str());
+    accountStorage->Insert(userID, newAccount);
+}
+
+
+// ============ 删除账户 ============
+void AccountSystem::deleteAccount(const string& userID) {
+    if (!isValidUsername(userID)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    if (!hasPrivilege(7)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+
+    vector<Account> nowExisting = accountStorage->Find(userID);
+    if (nowExisting.empty()) {
+        std::cout << "Invalid\n";
+        return;
+    }
+
+    if (isUserLoggedIn(userID)) {
+        std::cout << "Invalid\n";
+        return;
+    }
+    accountStorage->Delete(userID, nowExisting[0]);
+}
